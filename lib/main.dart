022447b9195d6844a21f14bd9ff72322ba8ae65c
antukbala <antukbala@gmail.com>
2021-11-26 20:57:25 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Calculator',
+            style: TextStyle(
+              fontSize: 32,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
